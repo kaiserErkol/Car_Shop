5b6ac7408cb58e7ab50b6daf6a6f73db266dcebb
kaiserErkol <kaiserErkol@users.noreply.github.com>
2024-02-14 09:10:53 +0100

    create Swift Project

---
+++ Car_Shop/Car_Shop/Car_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_ShopApp.swift
+//  Car_Shop
+//
+//  Created by MacBook on 14.02.24.
+//
+
+import SwiftUI
+
+@main
+struct Car_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
